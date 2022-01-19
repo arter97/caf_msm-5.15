85d764c968c9d8220919ca4520cb20bae9d7743a
Vatte Vishnuvardhan Reddy <quic_vishnuvr@quicinc.com>
2022-01-19 17:19:35 +0530

    net: stmmac: Managing frames with preamble suppressed on KSZ9131
    For 10M speed on KSZ9131RNX PHY all 7 bytes of preamble are
    removed and submitted to MAC. Configure PHY registers accordingly
    to manage such frames.
    
    Change-Id: I10f6db589502e53b86e0f92f3ff0faa4e5f6fcfa
    Signed-off-by: Vatte Vishnuvardhan Reddy <quic_vishnuvr@quicinc.com>

--- drivers/net/ethernet/stmicro/stmmac/stmmac_main.c
+++ drivers/net/ethernet/stmicro/stmmac/stmmac_main.c
@@ -49,6 +49,7 @@
 #include "dwmac1000.h"
 #include "dwxgmac2.h"
 #include "hwif.h"
+#include <linux/micrel_phy.h>
 
 /* As long as the interface is active, we keep the timestamping counter enabled
  * with fine resolution and binary rollover. This avoid non-monotonic behavior
@@ -59,6 +60,7 @@
 
 #define	STMMAC_ALIGN(x)		ALIGN(ALIGN(x, SMP_CACHE_BYTES), 16)
 #define	TSO_MAX_BUFF_SIZE	(SZ_16K - 1)
+#define KSZ9131RNX_LBR		0x11
 
 /* Module parameters */
 #define TX_TIMEO	5000
@@ -1152,6 +1154,7 @@ static void stmmac_mac_link_up(struct phylink_config *config,
 {
 	struct stmmac_priv *priv = netdev_priv(to_net_dev(config->dev));
 	u32 ctrl;
+	int phy_data = 0;
 
 	ctrl = readl(priv->ioaddr + MAC_CTRL_REG);
 	ctrl &= ~priv->hw->link.speed_mask;
@@ -1216,6 +1219,14 @@ static void stmmac_mac_link_up(struct phylink_config *config,
 	}
 
 	priv->speed = speed;
+
+	if (priv->speed == SPEED_10 &&
+	    duplex &&
+	   ((priv->phydev->phy_id & priv->phydev->drv->phy_id_mask) == PHY_ID_KSZ9131)) {
+		phy_data = priv->mii->read(priv->mii, priv->plat->phy_addr, KSZ9131RNX_LBR);
+		phy_data = phy_data | (1 << 2);
+		priv->mii->write(priv->mii, priv->plat->phy_addr, KSZ9131RNX_LBR, phy_data);
+	}
 
 	if (priv->plat->fix_mac_speed)
 		priv->plat->fix_mac_speed(priv->plat->bsp_priv, speed);
