4e774e66dcc43c2a6a765847d9166fc785826914
Riccardo Paolo Bestetti <pbl@bestov.io>
2022-06-17 10:54:35 +0200

    ipv4: ping: fix bind address validity check
    Commit 8ff978b8b222 ("ipv4/raw: support binding to nonlocal addresses")
    introduced a helper function to fold duplicated validity checks of bind
    addresses into inet_addr_valid_or_nonlocal(). However, this caused an
    unintended regression in ping_check_bind_addr(), which previously would
    reject binding to multicast and broadcast addresses, but now these are
    both incorrectly allowed as reported in [1].
    
    This patch restores the original check. A simple reordering is done to
    improve readability and make it evident that multicast and broadcast
    addresses should not be allowed. Also, add an early exit for INADDR_ANY
    which replaces missing behavior added by commit 0ce779a9f501 ("net: Avoid
    unnecessary inet_addr_type() call when addr is INADDR_ANY").
    
    Furthermore, this patch introduces regression selftests to catch these
    specific cases.
    
    [1] https://lore.kernel.org/netdev/CANP3RGdkAcDyAZoT1h8Gtuu0saq+eOrrTiWbxnOs+5zn+cpyKg@mail.gmail.com/.
    
    Fixes: 8ff978b8b222 ("ipv4/raw: support binding to nonlocal addresses")
    Change-Id: Idd37eba54d70dbf3efedac6b1c3d308bada6d362
    Cc: Miaohe Lin <linmiaohe@huawei.com>
    Reported-by: Maciej Żenczykowski <maze@google.com>
    Signed-off-by: Carlos Llamas <cmllamas@google.com>
    Signed-off-by: Riccardo Paolo Bestetti <pbl@bestov.io>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Git-commit: b4a028c4d031c27704ad73b1195ca69a1206941e
    Git-repo: https://kernel.googlesource.com/pub/scm/linux/kernel/git/stable/linux
    Signed-off-by: Kaustubh Pandey <kapandey@qti.qualcomm.com>

--- net/ipv4/ping.c
+++ net/ipv4/ping.c
@@ -305,6 +305,7 @@ static int ping_check_bind_addr(struct sock *sk, struct inet_sock *isk,
 	struct net *net = sock_net(sk);
 	if (sk->sk_family == AF_INET) {
 		struct sockaddr_in *addr = (struct sockaddr_in *) uaddr;
+		u32 tb_id = RT_TABLE_LOCAL;
 		int chk_addr_ret;
 
 		if (addr_len < sizeof(*addr))
@@ -318,11 +319,16 @@ static int ping_check_bind_addr(struct sock *sk, struct inet_sock *isk,
 		pr_debug("ping_check_bind_addr(sk=%p,addr=%pI4,port=%d)\n",
 			 sk, &addr->sin_addr.s_addr, ntohs(addr->sin_port));
 
-		chk_addr_ret = inet_addr_type(net, addr->sin_addr.s_addr);
+		if (addr->sin_addr.s_addr == htonl(INADDR_ANY))
+			return 0;
 
-		if (!inet_addr_valid_or_nonlocal(net, inet_sk(sk),
-					         addr->sin_addr.s_addr,
-	                                         chk_addr_ret))
+		tb_id = l3mdev_fib_table_by_index(net, sk->sk_bound_dev_if) ? : tb_id;
+		chk_addr_ret = inet_addr_type_table(net, addr->sin_addr.s_addr, tb_id);
+
+		if (chk_addr_ret == RTN_MULTICAST ||
+		    chk_addr_ret == RTN_BROADCAST ||
+		    (chk_addr_ret != RTN_LOCAL &&
+		     !inet_can_nonlocal_bind(net, isk)))
 			return -EADDRNOTAVAIL;
 
 #if IS_ENABLED(CONFIG_IPV6)
