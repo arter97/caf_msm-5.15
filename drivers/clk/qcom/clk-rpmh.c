arter97/caf_msm-5.4

clk: qcom: rpmh: Add support for RPMH clocks for lemans
Add clock rpmh data to communicate with RPMH clocks for
lemans.

Change-Id: I81b72627992e9645e791970c5ca24b32d029e280
Signed-off-by: Veera Vegivada <quic_vvegivad@quicinc.com>

drivers/clk/qcom/clk-rpmh.c
@@ -614,6 +614,22 @@ static const struct clk_rpmh_desc clk_rpmh_sdx55 = {
 	.num_clks = ARRAY_SIZE(sdx55_rpmh_clocks),
 };
 
+static struct clk_hw *lemans_rpmh_clocks[] = {
+	[RPMH_CXO_CLK]		= &lahaina_bi_tcxo.hw,
+	[RPMH_CXO_CLK_A]	= &lahaina_bi_tcxo_ao.hw,
+	[RPMH_LN_BB_CLK1]	= &lahaina_ln_bb_clk1.hw,
+	[RPMH_LN_BB_CLK2]	= &lahaina_ln_bb_clk2.hw,
+	[RPMH_LN_BB_CLK2_A]	= &lahaina_ln_bb_clk2_ao.hw,
+	[RPMH_IPA_CLK]		= &lahaina_ipa.hw,
+	[RPMH_PKA_CLK]		= &lahaina_pka.hw,
+	[RPMH_HWKM_CLK]		= &lahaina_hwkm.hw,
+};
+
+static const struct clk_rpmh_desc clk_rpmh_lemans = {
+	.clks = lemans_rpmh_clocks,
+	.num_clks = ARRAY_SIZE(lemans_rpmh_clocks),
+};
+
 static struct clk_hw *of_clk_rpmh_hw_get(struct of_phandle_args *clkspec,
 					 void *data)
 {
@@ -715,6 +731,7 @@ static const struct of_device_id clk_rpmh_match_table[] = {
 	{ .compatible = "qcom,yupik-rpmh-clk", .data = &clk_rpmh_yupik},
 	{ .compatible = "qcom,direwolf-rpmh-clk", .data = &clk_rpmh_direwolf},
 	{ .compatible = "qcom,sdx55-rpmh-clk",  .data = &clk_rpmh_sdx55},
+	{ .compatible = "qcom,lemans-rpmh-clk", .data = &clk_rpmh_lemans},
 	{ }
 };
 MODULE_DEVICE_TABLE(of, clk_rpmh_match_table);

arter97/caf_msm-5.15

drivers/clk/qcom/clk-rpmh.c
@@ -700,6 +700,22 @@ static const struct clk_rpmh_desc clk_rpmh_sdxpinn = {
 	.num_clks = ARRAY_SIZE(sdxpinn_rpmh_clocks),
 };
 
+static struct clk_hw *lemans_rpmh_clocks[] = {
+	[RPMH_CXO_CLK]		= &sdm845_bi_tcxo.hw,
+	[RPMH_CXO_CLK_A]	= &sdm845_bi_tcxo_ao.hw,
+	[RPMH_LN_BB_CLK1]	= &sm8250_ln_bb_clk1.hw,
+	[RPMH_LN_BB_CLK2]	= &sdm845_ln_bb_clk2.hw,
+	[RPMH_LN_BB_CLK2_A]	= &sdm845_ln_bb_clk2_ao.hw,
+	[RPMH_IPA_CLK]		= &sdm845_ipa.hw,
+	[RPMH_PKA_CLK]		= &sm8350_pka.hw,
+	[RPMH_HWKM_CLK]		= &sm8350_hwkm.hw,
+};
+
+static const struct clk_rpmh_desc clk_rpmh_lemans = {
+	.clks = lemans_rpmh_clocks,
+	.num_clks = ARRAY_SIZE(lemans_rpmh_clocks),
+};
+
 static struct clk_hw *of_clk_rpmh_hw_get(struct of_phandle_args *clkspec,
 					 void *data)
 {
@@ -805,6 +821,7 @@ static const struct of_device_id clk_rpmh_match_table[] = {
 	{ .compatible = "qcom,kalama-rpmh-clk", .data = &clk_rpmh_kalama},
 	{ .compatible = "qcom,cinder-rpmh-clk", .data = &clk_rpmh_cinder},
 	{ .compatible = "qcom,sdxpinn-rpmh-clk", .data = &clk_rpmh_sdxpinn},
+	{ .compatible = "qcom,lemans-rpmh-clk", .data = &clk_rpmh_lemans},
 	{ }
 };
 MODULE_DEVICE_TABLE(of, clk_rpmh_match_table);