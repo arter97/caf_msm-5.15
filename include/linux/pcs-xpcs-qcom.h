96e839f83cdb9e64631a2cff0ad935cabe99920d
Manuel Gomez <quic_gomez@quicinc.com>
2022-11-07 14:00:30 -0800

    include: linux: pcs: Update API declarations
    Remove speed_1g parameter for serdes loopback enablement since
    new SerDes block is now being used and is no longer requiring
    extra reset delays. Update parameters for both interrupt handlers.
    Add irq number for PCS interrupts.
    
    Change-Id: I56f3a4c431cdbf03c474bc04406ca539da96fef5
    Signed-off-by: Manuel Gomez <quic_gomez@quicinc.com>

--- include/linux/pcs-xpcs-qcom.h
+++ include/linux/pcs-xpcs-qcom.h
@@ -23,8 +23,10 @@ struct dw_xpcs_qcom {
 	const struct xpcs_id *id;
 	struct phylink_pcs pcs;
 	void __iomem *addr;
+	int pcs_intr;
 };
 
+#if IS_ENABLED(CONFIG_PCS_QCOM)
 int qcom_xpcs_get_an_mode(struct dw_xpcs_qcom *xpcs,
 			  phy_interface_t interface);
 void qcom_xpcs_link_up(struct phylink_pcs *pcs, unsigned int mode,
@@ -37,17 +39,89 @@ int qcom_xpcs_config_eee(struct dw_xpcs_qcom *xpcs, int mult_fact_100ns,
 struct dw_xpcs_qcom *qcom_xpcs_create(void __iomem *addr,
 				      phy_interface_t interface);
 void qcom_xpcs_destroy(struct dw_xpcs_qcom *xpcs);
-void xpcs_link_up_usxgmii(struct dw_xpcs_qcom *xpcs, int speed);
-void xpcs_link_up_sgmii(struct dw_xpcs_qcom *xpcs, unsigned int mode,
-			int speed, int duplex);
-int qcom_xpcs_poll_reset(struct dw_xpcs_qcom *xpcs, unsigned int offset,
-			 unsigned int field);
 const struct xpcs_compat *xpcs_find_compat(const struct xpcs_id *id,
 					   phy_interface_t interface);
-int qcom_xpcs_enable_pcs_loopback(struct dw_xpcs_qcom *xpcs);
-int qcom_xpcs_disable_pcs_loopback(struct dw_xpcs_qcom *xpcs);
-int qcom_xpcs_enable_serdes_loopback(struct dw_xpcs_qcom *xpcs, bool speed_1g);
-int qcom_xpcs_disable_serdes_loopback(struct dw_xpcs_qcom *xpcs);
-void qcom_xpcs_handle_an_intr(struct work_struct *work);
-int qcom_xpcs_check_aneg_ioc(void);
+int qcom_xpcs_serdes_loopback(struct dw_xpcs_qcom *xpcs, bool on);
+int qcom_xpcs_pcs_loopback(struct dw_xpcs_qcom *xpcs, bool on);
+int qcom_xpcs_check_aneg_ioc(struct dw_xpcs_qcom *xpcs);
+irqreturn_t ethqos_xpcs_isr(int irq, void *dev_data);
+int ethqos_xpcs_intr_config(phy_interface_t interface);
+int ethqos_xpcs_intr_enable(void);
+int ethqos_xpcs_init(phy_interface_t mode);
+#else /* IS_ENABLED(CONFIG_PCS_QCOM) */
+static inline int qcom_xpcs_get_an_mode(struct dw_xpcs_qcom *xpcs,
+					phy_interface_t interface)
+{
+	return -EINVAL;
+}
+
+static inline void qcom_xpcs_link_up(struct phylink_pcs *pcs, unsigned int mode,
+				     phy_interface_t interface, int speed,
+				     int duplex)
+{
+}
+
+static inline void qcom_xpcs_validate(struct dw_xpcs_qcom *xpcs,
+				      unsigned long *supported,
+				      struct phylink_link_state *state)
+{
+}
+
+static inline int qcom_xpcs_config_eee(struct dw_xpcs_qcom *xpcs, int mult_fact_100ns,
+				       int enable)
+{
+	return -EINVAL;
+}
+
+static inline struct dw_xpcs_qcom *qcom_xpcs_create(void __iomem *addr,
+						    phy_interface_t interface)
+{
+	return NULL;
+}
+
+static inline void qcom_xpcs_destroy(struct dw_xpcs_qcom *xpcs)
+{
+}
+
+static inline const struct xpcs_compat *xpcs_find_compat(const struct xpcs_id *id,
+							 phy_interface_t interface)
+{
+	return NULL;
+}
+
+static inline int qcom_xpcs_serdes_loopback(struct dw_xpcs_qcom *xpcs, bool on)
+{
+	return -EINVAL;
+}
+
+static inline int qcom_xpcs_pcs_loopback(struct dw_xpcs_qcom *xpcs, bool on)
+{
+	return -EINVAL;
+}
+
+static inline int qcom_xpcs_check_aneg_ioc(struct dw_xpcs_qcom *xpcs)
+{
+	return -EINVAL;
+}
+
+static inline irqreturn_t ethqos_xpcs_isr(int irq, void *dev_data)
+{
+	return IRQ_NONE;
+}
+
+static inline int ethqos_xpcs_intr_config(phy_interface_t interface)
+{
+	return -EINVAL;
+}
+
+static inline int ethqos_xpcs_intr_enable(void)
+{
+	return -EINVAL;
+}
+
+static inline int ethqos_xpcs_init(phy_interface_t mode)
+{
+	return 0;
+}
+#endif /* IS_ENABLED(CONFIG_PCS_QCOM) */
 #endif /* __LINUX_PCS_XPCS_QCOM_H */
