e675cfa3d3a813590caff9135328628760a76cb0
Rohan Dutta <quic_drohan@quicinc.com>
2022-11-15 23:33:55 +0530

    wifi: cfg80211: Add link_id parameter to cfg80211_disconnected call for MLO
    Add support for cfg80211_disconnected to have the link_id specified as a
    new parameter.
    
    Pass link_id from the underlying driver to the userspace for link
    removal feature as part of ML Reconfig. The link_id will be passed
    to the userspace in NL80211_CMD_DISCONNECT in NL80211_ATTR_MLO_LINK_ID
    attribute, the userspace in station mode will remove the specified
    link from MLD, for complete disconnect the driver should indicate
    link_id as unspecified, for legacy and non-ML connections too the
    driver should not specify the link-id and keep it unspecified.
    
    Change-Id: I3b2bbd649b18b2fa7c3e37611b6c2d282e372f2c
    Signed-off-by: Rohan Dutta <quic_drohan@quicinc.com>
    Signed-off-by: Pooventhiran G <quic_pooventh@quicinc.com>

--- net/wireless/nl80211.h
+++ net/wireless/nl80211.h
@@ -86,9 +86,16 @@ void nl80211_send_roamed(struct cfg80211_registered_device *rdev,
 			 struct cfg80211_roam_info *info, gfp_t gfp);
 void nl80211_send_port_authorized(struct cfg80211_registered_device *rdev,
 				  struct net_device *netdev, const u8 *bssid);
+#ifndef CFG80211_PROP_MULTI_LINK_SUPPORT
 void nl80211_send_disconnected(struct cfg80211_registered_device *rdev,
 			       struct net_device *netdev, u16 reason,
 			       const u8 *ie, size_t ie_len, bool from_ap);
+#else /* CFG80211_PROP_MULTI_LINK_SUPPORT */
+void nl80211_send_disconnected(struct cfg80211_registered_device *rdev,
+			       struct net_device *netdev, u16 reason,
+			       const u8 *ie, size_t ie_len, bool from_ap,
+			       int link_id);
+#endif /* CFG80211_PROP_MULTI_LINK_SUPPORT */
 
 void
 nl80211_michael_mic_failure(struct cfg80211_registered_device *rdev,
