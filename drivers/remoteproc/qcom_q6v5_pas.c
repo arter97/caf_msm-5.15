b821677c4f0348b2bdfbaf80f686d74e4a91397d
quic_ysaispur <quic_ysaispur@quicinc.com>
2022-12-14 10:33:19 -0800

    Revert "remoteproc: qcom: Change coredump registration for pas driver"
    This reverts commit 7a95bc1869999ec1a30e8837fc671d730353ff02.
    
    Change-Id: I70d8d82450c9b2f0ca3126728d34b256d6f20e99
    Signed-off-by: Spurthy Yadagiri <quic_ysaispur@quicinc.com>

--- drivers/remoteproc/qcom_q6v5_pas.c
+++ drivers/remoteproc/qcom_q6v5_pas.c
@@ -579,7 +579,11 @@ static int adsp_attach(struct rproc *rproc)
 		goto begin_attach;
 	}
 
-	adsp_add_coredump_segments(adsp, fw);
+	ret = qcom_register_dump_segments(rproc, fw);
+	if (ret) {
+		dev_err(adsp->dev, "Failed to register dump segments\n");
+		dev_err(adsp->dev, "Dumps will not be available\n");
+	}
 	release_firmware(fw);
 
 begin_attach:
@@ -701,6 +705,7 @@ static const struct rproc_ops adsp_minidump_ops = {
 	.start = adsp_start,
 	.stop = adsp_stop,
 	.da_to_va = adsp_da_to_va,
+	.parse_fw = qcom_register_dump_segments,
 	.load = adsp_load,
 	.panic = adsp_panic,
 	.coredump = adsp_minidump,
