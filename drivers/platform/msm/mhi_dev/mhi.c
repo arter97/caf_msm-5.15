nekoshirro/platform_kernel_xiaomi_marble

msm: mhi-dev: Release and init edma channels in mhi suspend and resume
Edma block gets reset when pcie goes to D3Cold.
So, When the device is going to suspend release all the channels
and while resuming reinit the edma channels.

Change-Id: I050dbf0b523bc5b781d55e335b1ade51dd65d19e
Signed-off-by: Krishna chaitanya chundru <quic_krichai@quicinc.com>

drivers/platform/msm/mhi_dev/mhi.c
@@ -4402,21 +4402,45 @@ static void mhi_dev_pcie_handle_event(struct work_struct *work)
 	}
 }
 
-static int mhi_edma_init(struct device *dev)
+int mhi_edma_release(void)
 {
-	mhi_ctx->tx_dma_chan = dma_request_slave_channel(dev, "tx");
-	if (IS_ERR_OR_NULL(mhi_ctx->tx_dma_chan)) {
-		pr_err("%s(): request for TX chan failed\n", __func__);
-		return -EIO;
-	}
+	dma_release_channel(mhi_ctx->tx_dma_chan);
+	mhi_ctx->tx_dma_chan = NULL;
+	dma_release_channel(mhi_ctx->rx_dma_chan);
+	mhi_ctx->rx_dma_chan = NULL;
+	return 0;
+}
 
+int mhi_edma_status(void)
+{
+	int ret = 0;
+
+	if (dmaengine_tx_status(mhi_ctx->tx_dma_chan, 0, NULL) == DMA_IN_PROGRESS)
+		ret = -EBUSY;
+	if (dmaengine_tx_status(mhi_ctx->rx_dma_chan, 0, NULL) == DMA_IN_PROGRESS)
+		ret = -EBUSY;
+
+	return ret;
+}
+
+int mhi_edma_init(struct device *dev)
+{
+	if (!mhi_ctx->tx_dma_chan) {
+		mhi_ctx->tx_dma_chan = dma_request_slave_channel(dev, "tx");
+		if (IS_ERR_OR_NULL(mhi_ctx->tx_dma_chan)) {
+			pr_err("%s(): request for TX chan failed\n", __func__);
+			return -EIO;
+		}
+	}
 	mhi_log(MHI_MSG_VERBOSE, "request for TX chan returned :%pK\n",
 			mhi_ctx->tx_dma_chan);
 
-	mhi_ctx->rx_dma_chan = dma_request_slave_channel(dev, "rx");
-	if (IS_ERR_OR_NULL(mhi_ctx->rx_dma_chan)) {
-		pr_err("%s(): request for RX chan failed\n", __func__);
-		return -EIO;
+	if (!mhi_ctx->rx_dma_chan) {
+		mhi_ctx->rx_dma_chan = dma_request_slave_channel(dev, "rx");
+		if (IS_ERR_OR_NULL(mhi_ctx->rx_dma_chan)) {
+			pr_err("%s(): request for RX chan failed\n", __func__);
+			return -EIO;
+		}
 	}
 	mhi_log(MHI_MSG_VERBOSE, "request for RX chan returned :%pK\n",
 			mhi_ctx->rx_dma_chan);

arter97/caf_msm-5.15

Signed-off-by: Vivek Sharma <quic_sharmav@quicinc.com>
Signed-off-by: Shivangi Yadav <quic_shivyada@quicinc.com>

drivers/platform/msm/mhi_dev/mhi.c
@@ -4957,22 +4957,47 @@ static void mhi_dev_setup_virt_device(struct mhi_dev_ctx *mhictx)
 	}
 }
 
-static int mhi_edma_init(struct mhi_dev_ctx *mhi_hw_ctx, struct device *dev)
+int mhi_edma_release(void)
 {
-	mhi_hw_ctx->tx_dma_chan = dma_request_slave_channel(dev, "tx");
-	if (IS_ERR_OR_NULL(mhi_hw_ctx->tx_dma_chan)) {
-		mhi_log(MHI_MSG_ERROR, "request for TX ch failed\n");
-		return -EIO;
-	}
+	dma_release_channel(mhi_hw_ctx->tx_dma_chan);
 
-	mhi_log(MHI_MSG_VERBOSE, "request for TX ch returned :%pK\n",
+	mhi_hw_ctx->tx_dma_chan = NULL;
+	dma_release_channel(mhi_hw_ctx->rx_dma_chan);
+	mhi_hw_ctx->rx_dma_chan = NULL;
+	return 0;
+}
+
+int mhi_edma_status(void)
+{
+	int ret = 0;
+
+	if (dmaengine_tx_status(mhi_hw_ctx->tx_dma_chan, 0, NULL) == DMA_IN_PROGRESS)
+		ret = -EBUSY;
+	if (dmaengine_tx_status(mhi_hw_ctx->rx_dma_chan, 0, NULL) == DMA_IN_PROGRESS)
+		ret = -EBUSY;
+
+	return ret;
+}
+
+int mhi_edma_init(struct device *dev)
+{
+	if (!mhi_hw_ctx->tx_dma_chan) {
+		mhi_hw_ctx->tx_dma_chan = dma_request_slave_channel(dev, "tx");
+		if (IS_ERR_OR_NULL(mhi_hw_ctx->tx_dma_chan)) {
+			pr_err("%s(): request for TX chan failed\n", __func__);
+			return -EIO;
+		}
+	}
+	mhi_log(MHI_MSG_VERBOSE, "request for TX chan returned :%pK\n",
 			mhi_hw_ctx->tx_dma_chan);
 
-	mhi_hw_ctx->rx_dma_chan = dma_request_slave_channel(dev, "rx");
-	if (IS_ERR_OR_NULL(mhi_hw_ctx->rx_dma_chan)) {
-		mhi_log(MHI_MSG_ERROR, "request for RX ch failed\n");
-		return -EIO;
-	}
+	if (!mhi_hw_ctx->rx_dma_chan) {
+		mhi_hw_ctx->rx_dma_chan = dma_request_slave_channel(dev, "rx");
+		if (IS_ERR_OR_NULL(mhi_hw_ctx->rx_dma_chan)) {
+			pr_err("%s(): request for RX chan failed\n", __func__);
+			return -EIO;
+		}
+}
 	mhi_log(MHI_MSG_VERBOSE, "request for RX ch returned :%pK\n",
 			mhi_hw_ctx->rx_dma_chan);
 	return 0;
@@ -5017,7 +5042,7 @@ static int mhi_dev_probe(struct platform_device *pdev)
 	}
 
 	if (mhi_pf->use_edma) {
-		rc = mhi_edma_init(mhi_hw_ctx, &pdev->dev);
+		rc = mhi_edma_init(&pdev->dev);
 		if (rc) {
 			mhi_log(MHI_MSG_ERROR,
 				"MHI: mhi edma init failed, rc = %d\n", rc);