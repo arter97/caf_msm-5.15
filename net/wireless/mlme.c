goytin/linaro-linux-ipq-5.4

nl80211: Allow transmitting auth frames with random TA
Allow authentication frames with a random local address as transmit
address if the driver supports.

This is required for below cases:
- Enabling use of randomized transmit address for PASN authentication
  frames improve privacy of WLAN clients.
- To allow user space to use link address indicated by driver as
  transmit address for authentication frames triggered by
  NL80211_CMD_EXTERNAL_AUTH for MLO connection.

Change-Id: I7f6fe8b1b9a5145fd56da3684249d5adcdbc04d0
Signed-off-by: Sai Pratyusha Magam <quic_smagam@quicinc.com>

net/wireless/mlme.c
@@ -673,25 +673,40 @@ int cfg80211_mlme_mgmt_tx(struct cfg80211_registered_device *rdev,
 	}
 
 	if (!ether_addr_equal(mgmt->sa, wdev_address(wdev))) {
-		/* Allow random TA to be used with Public Action frames if the
-		 * driver has indicated support for this. Otherwise, only allow
-		 * the local address to be used.
-		 */
-		if (!ieee80211_is_action(mgmt->frame_control) ||
-		    mgmt->u.action.category != WLAN_CATEGORY_PUBLIC)
-			return -EINVAL;
-		if (!wdev->current_bss &&
-		    !wiphy_ext_feature_isset(
-			    &rdev->wiphy,
-			    NL80211_EXT_FEATURE_MGMT_TX_RANDOM_TA))
-			return -EINVAL;
-		if (wdev->current_bss &&
-		    !wiphy_ext_feature_isset(
-			    &rdev->wiphy,
-			    NL80211_EXT_FEATURE_MGMT_TX_RANDOM_TA_CONNECTED))
-			return -EINVAL;
-	}
 
+		switch (ieee80211_is_auth(mgmt->frame_control)) {
+		case 0:
+			/* Allow random TA to be used with Public Action frames if the
+			 * driver has indicated support for this. Otherwise, only allow
+			 * the local address to be used.
+			 */
+			if (!ieee80211_is_action(mgmt->frame_control) ||
+			    mgmt->u.action.category != WLAN_CATEGORY_PUBLIC)
+				return -EINVAL;
+			if (!wdev->current_bss &&
+			    !wiphy_ext_feature_isset(
+				    &rdev->wiphy,
+				    NL80211_EXT_FEATURE_MGMT_TX_RANDOM_TA))
+				return -EINVAL;
+			if (wdev->current_bss &&
+			    !wiphy_ext_feature_isset(
+				    &rdev->wiphy,
+				    NL80211_EXT_FEATURE_MGMT_TX_RANDOM_TA_CONNECTED))
+				return -EINVAL;
+		break;
+		case 1:
+			/* Allow random TA to be used with authentication frames if the
+			 * driver has indicated support for this. Otherwise, only allow
+			 * the local address to be used.
+			 */
+			if (!wiphy_ext_feature_isset(&rdev->wiphy,
+						     NL80211_EXT_FEATURE_AUTH_TX_RANDOM_TA))
+				return -EINVAL;
+		break;
+		default:
+			return -EINVAL;
+		}
+	}
 	/* Transmit the Action frame as requested by user space */
 	return rdev_mgmt_tx(rdev, wdev, params, cookie);
 }

arter97/caf_msm-5.15

Signed-off-by: Pooventhiran G <quic_pooventh@quicinc.com>

net/wireless/mlme.c
@@ -736,23 +736,31 @@ int cfg80211_mlme_mgmt_tx(struct cfg80211_registered_device *rdev,
 	}
 
 	if (!ether_addr_equal(mgmt->sa, wdev_address(wdev))) {
-		/* Allow random TA to be used with Public Action frames if the
-		 * driver has indicated support for this. Otherwise, only allow
-		 * the local address to be used.
-		 */
-		if (!ieee80211_is_action(mgmt->frame_control) ||
-		    mgmt->u.action.category != WLAN_CATEGORY_PUBLIC)
-			return -EINVAL;
-		if (!wdev->connected &&
-		    !wiphy_ext_feature_isset(
-			    &rdev->wiphy,
-			    NL80211_EXT_FEATURE_MGMT_TX_RANDOM_TA))
-			return -EINVAL;
-		if (wdev->connected &&
-		    !wiphy_ext_feature_isset(
-			    &rdev->wiphy,
-			    NL80211_EXT_FEATURE_MGMT_TX_RANDOM_TA_CONNECTED))
-			return -EINVAL;
+		if (!ieee80211_is_auth(mgmt->frame_control)) {
+			/* Allow random TA to be used with Public Action frames if the
+			 * driver has indicated support for this. Otherwise, only allow
+			 * the local address to be used.
+			 */
+			if (!ieee80211_is_action(mgmt->frame_control) ||
+			    mgmt->u.action.category != WLAN_CATEGORY_PUBLIC)
+				return -EINVAL;
+			if (!wdev->connected &&
+			    !wiphy_ext_feature_isset(&rdev->wiphy,
+				    NL80211_EXT_FEATURE_MGMT_TX_RANDOM_TA))
+				return -EINVAL;
+			if (wdev->connected &&
+			    !wiphy_ext_feature_isset(&rdev->wiphy,
+				    NL80211_EXT_FEATURE_MGMT_TX_RANDOM_TA_CONNECTED))
+				return -EINVAL;
+		} else {
+			/* Allow random TA to be used with authentication frames if the
+			 * driver has indicated support for this. Otherwise, only allow
+			 * the local address to be used.
+			 */
+			if (!wiphy_ext_feature_isset(&rdev->wiphy,
+						     NL80211_EXT_FEATURE_AUTH_TX_RANDOM_TA))
+				return -EINVAL;
+		}
 	}
 
 	/* Transmit the management frame as requested by user space */