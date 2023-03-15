a48366496165eb3c33d1dbaef62b03d86d01d8c0
Nishant Pandey <quic_nishpand@quicinc.com>
2023-03-14 22:37:16 -0700

    qcom: boot markers: Add system resume boot kpi marker
    "start system resume" kpi marker is reference time to get
    peripheral out of suspend like usb, pcie and ethernet.
    
    Change-Id: If2f06533bf3597ab53453e10de270e7ff217a4ef
    Signed-off-by: Nishant Pandey <quic_nishpand@quicinc.com>

--- kernel/power/suspend.c
+++ kernel/power/suspend.c
@@ -31,6 +31,7 @@
 #include <linux/compiler.h>
 #include <linux/moduleparam.h>
 #include <linux/wakeup_reason.h>
+#include <soc/qcom/boot_stats.h>
 
 #include "power.h"
 
@@ -449,6 +450,7 @@ static int suspend_enter(suspend_state_t state, bool *wakeup)
 			trace_suspend_resume(TPS("machine_suspend"),
 				state, true);
 			error = suspend_ops->enter(state);
+			update_marker("M - Start System Resume");
 			trace_suspend_resume(TPS("machine_suspend"),
 				state, false);
 		} else if (*wakeup) {
