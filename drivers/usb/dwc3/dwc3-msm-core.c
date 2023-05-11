983fa6cbf8446f73113c379efb9f0f1c27bcfeed
QU TAO <quic_taoq@quicinc.com>
2023-05-10 22:10:20 -0700

    Revert "usb: dwc3-msm: Add reboot handler in dwc3 msm"
    This reverts commit 7926e22bb78ce88de59b527850fcaad1970217f7.
    
    Change-Id: I387f3ac8ceb9fe824427dc3167df89cb920471a6
    Signed-off-by: QU TAO <quic_taoq@quicinc.com>

--- drivers/usb/dwc3/dwc3-msm-core.c
+++ drivers/usb/dwc3/dwc3-msm-core.c
@@ -6158,13 +6158,6 @@ static int dwc3_msm_remove(struct platform_device *pdev)
 	return 0;
 }
 
-static void dwc3_msm_shutdown(struct platform_device *pdev)
-{
-	pr_err("dwc3 msm reboot handler");
-	dwc3_msm_remove(pdev);
-
-}
-
 static int dwc3_msm_host_ss_powerdown(struct dwc3_msm *mdwc)
 {
 	u32 reg;
@@ -7047,7 +7040,6 @@ MODULE_DEVICE_TABLE(of, of_dwc3_matach);
 static struct platform_driver dwc3_msm_driver = {
 	.probe		= dwc3_msm_probe,
 	.remove		= dwc3_msm_remove,
-	.shutdown	= dwc3_msm_shutdown,
 	.driver		= {
 		.name	= "msm-dwc3",
 		.pm	= &dwc3_msm_dev_pm_ops,
