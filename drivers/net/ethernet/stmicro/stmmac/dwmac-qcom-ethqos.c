233aeca4badb9656d4f6ce133d8a59814463a709
Lakshit Tyagi <quic_ltyagi@quicinc.com>
2023-06-07 22:47:22 +0530

    net: stmmac: Default value of passthrough node
    Set default value of passthrough_en sysfs node to 1
    when early ethernet is enabled and to 0 when it is disabled.
    
    Change-Id: I066765a8dd80edb55bbf492ac076426d406975d3
    Signed-off-by: Lakshit Tyagi <quic_ltyagi@quicinc.com>

--- drivers/net/ethernet/stmicro/stmmac/dwmac-qcom-ethqos.c
+++ drivers/net/ethernet/stmicro/stmmac/dwmac-qcom-ethqos.c
@@ -5809,7 +5809,10 @@ static int qcom_ethqos_probe(struct platform_device *pdev)
 
 
 #if IS_ENABLED(CONFIG_ETHQOS_QCOM_HOSTVM)
-	ethqos->passthrough_en = 0;
+	if (ethqos->early_eth_enabled)
+		ethqos->passthrough_en = 1;
+	else
+		ethqos->passthrough_en = 0;
 #else
 	ethqos->cv2x_priority = 0;
 #endif
