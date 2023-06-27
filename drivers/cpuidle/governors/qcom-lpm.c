772ab20046cf64e589f5d7f78d65970e6c2cc324
Tushar Nimkar <quic_tnimkar@quicinc.com>
2023-06-27 14:08:11 +0530

    cpuidle: qcom-lpm: Add a device enabled check before using it
    ipi_raise()/ipi_entry() can happen before cpu governor's enabe
    is called where it does necessary initialization.
    
    This changes adds check for device enable before accessing
    variables. This fixes issue where spinlock used before init done.
    BUG: spinlock bad magic on CPU#5, modprobe/527.
    
    Change-Id: I83f572a2d7d0b8e3b3fe58982f38ae58f69cacaf
    Signed-off-by: Tushar Nimkar <quic_tnimkar@quicinc.com>

--- drivers/cpuidle/governors/qcom-lpm.c
+++ drivers/cpuidle/governors/qcom-lpm.c
@@ -467,6 +467,9 @@ static void ipi_raise(void *ignore, const struct cpumask *mask, const char *unus
 
 	for_each_cpu(cpu, mask) {
 		cpu_gov = &(per_cpu(lpm_cpu_data, cpu));
+		if (!cpu_gov->enable)
+			return;
+
 		spin_lock_irqsave(&cpu_gov->lock, flags);
 		cpu_gov->ipi_pending = true;
 		spin_unlock_irqrestore(&cpu_gov->lock, flags);
@@ -485,6 +488,8 @@ static void ipi_entry(void *ignore, const char *unused)
 
 	cpu = raw_smp_processor_id();
 	cpu_gov = &(per_cpu(lpm_cpu_data, cpu));
+	if (!cpu_gov->enable)
+		return;
 
 	spin_lock_irqsave(&cpu_gov->lock, flags);
 	cpu_gov->ipi_pending = false;
@@ -675,6 +680,9 @@ static void lpm_idle_enter(void *unused, int *state, struct cpuidle_device *dev)
 	unsigned long flags;
 
 	if (*state == 0)
+		return;
+
+	if (!cpu_gov->enable)
 		return;
 
 	/* Restrict to WFI state if there is an IPI pending on current CPU */
