97a02a5a538c78a45318df3ed6178f160a83783d
Paras Sharma <quic_parass@quicinc.com>
2023-09-15 02:50:48 -0700

    pci: framework: Disable auto suspend link
    Some endpoint devices do not go into D3hot during suspend.
    By pass auto suspend if device do not allow D3hot. In order
    prevent the ABI breakage warp this QTI specific change under
    CONFIG_PCI_QTI.
    
    We are removing dependency of QGKI as its deprecated and
    not required for MDM branches.
    
    Change-Id: Ida6e4a2b60b7d08932bfff79144afd67787ca0f2
    Signed-off-by: Sujeev Dias <sdias@codeaurora.org>
    Signed-off-by: Hemant Kumar <hemantk@codeaurora.org>
    Signed-off-by: Tony Truong <truong@codeaurora.org>
    Signed-off-by: Subramanian Ananthanarayanan <quic_skananth@quicinc.com>
    Signed-off-by: Paras Sharma <quic_parass@quicinc.com>

--- drivers/pci/pci-driver.c
+++ drivers/pci/pci-driver.c
@@ -836,6 +836,10 @@ static int pci_pm_suspend_noirq(struct device *dev)
 		}
 	}
 
+	/* if d3hot is not supported bail out */
+	if (IS_ENABLED(CONFIG_PCI_QTI) && pci_dev->no_d3hot)
+		return 0;
+
 	if (pci_dev->skip_bus_pm) {
 		/*
 		 * Either the device is a bridge with a child in D0 below it, or
@@ -918,8 +922,10 @@ static int pci_pm_resume_noirq(struct device *dev)
 	 * configuration here and attempting to put them into D0 again is
 	 * pointless, so avoid doing that.
 	 */
-	if (!(skip_bus_pm && pm_suspend_no_platform()))
-		pci_pm_default_resume_early(pci_dev);
+	if (!(skip_bus_pm && pm_suspend_no_platform())) {
+		if (!IS_ENABLED(CONFIG_PCI_QTI) && !pci_dev->no_d3hot)
+			pci_pm_default_resume_early(pci_dev);
+	}
 
 	pci_fixup_device(pci_fixup_resume_early, pci_dev);
 	pcie_pme_root_status_cleanup(pci_dev);
@@ -1283,6 +1289,10 @@ static int pci_pm_runtime_suspend(struct device *dev)
 		return 0;
 	}
 
+	/* if d3hot is not supported bail out */
+	if (IS_ENABLED(CONFIG_PCI_QTI) && pci_dev->no_d3hot)
+		return 0;
+
 	if (!pci_dev->state_saved) {
 		pci_save_state(pci_dev);
 		pci_finish_runtime_suspend(pci_dev);
@@ -1297,6 +1307,10 @@ static int pci_pm_runtime_resume(struct device *dev)
 	const struct dev_pm_ops *pm = dev->driver ? dev->driver->pm : NULL;
 	pci_power_t prev_state = pci_dev->current_state;
 	int error = 0;
+
+	/* we skipped d3hot processing so skip re-init */
+	if (IS_ENABLED(CONFIG_PCI_QTI) && pci_dev->no_d3hot)
+		goto skip_restore;
 
 	/*
 	 * Restoring config space is necessary even if the device is not bound
@@ -1314,6 +1328,7 @@ static int pci_pm_runtime_resume(struct device *dev)
 	if (prev_state == PCI_D3cold)
 		pci_bridge_wait_for_secondary_bus(pci_dev, "resume", PCI_RESET_WAIT);
 
+skip_restore:
 	if (pm && pm->runtime_resume)
 		error = pm->runtime_resume(dev);
 
