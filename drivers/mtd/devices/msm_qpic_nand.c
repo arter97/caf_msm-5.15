5ea5e6cb517b09385ee5499c51a7ced375126e65
Madhusudhan Sana <quic_msana@quicinc.com>
2023-10-19 14:36:52 +0530

    mtd: msm_qpic_nand: Fix issues related to NULL check
    Add NULL check before using the memory.
    
    Change-Id: I7495a228ab316a490e72f2c28d0ab848455b7b80
    Signed-off-by: Madhusudhan Sana <quic_msana@quicinc.com>

--- drivers/mtd/devices/msm_qpic_nand.c
+++ drivers/mtd/devices/msm_qpic_nand.c
@@ -1856,6 +1856,8 @@ static int msm_nand_is_erased_page_ps(struct mtd_info *mtd, loff_t from,
 	memcpy(&raw_ops, ops, sizeof(struct mtd_oob_ops));
 	raw_ops.mode = MTD_OPS_RAW;
 	ecc = kzalloc(total_ecc_byte_cnt, GFP_KERNEL);
+	if (!ecc)
+		return -ENOMEM;
 
 	wait_event(chip->dma_wait_queue, (dma_buffer = msm_nand_get_dma_buffer(
 					chip, sizeof(*dma_buffer))));
@@ -2474,6 +2476,8 @@ static int msm_nand_is_erased_page(struct mtd_info *mtd, loff_t from,
 	memcpy(&raw_ops, ops, sizeof(struct mtd_oob_ops));
 	raw_ops.mode = MTD_OPS_RAW;
 	ecc = kzalloc(total_ecc_byte_cnt, GFP_KERNEL);
+	if (!ecc)
+		return -ENOMEM;
 
 	wait_event(chip->dma_wait_queue, (dma_buffer = msm_nand_get_dma_buffer(
 					chip, sizeof(*dma_buffer))));
