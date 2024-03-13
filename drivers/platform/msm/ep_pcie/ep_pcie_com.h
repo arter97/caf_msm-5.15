bec21c635a6f8220b5fd626002afdc340d467d2b
Qiang Yu <quic_qianyu@quicinc.com>
2024-03-12 20:27:22 -0700

    msm: ep_pcie: Disable hot reset and ignore linkdown
    Intel host tends to retrain the link if device doesn't send PM_Enter_L23
    in time, leading to linkdown and hot reset in device side. The hot reset
    here even results to PERST# timeout and device crash to PBL, which is
    not expected.
    
    Since we are in the entry of shutdown the link when linkdown happen, it
    makes sense to ignore the linkdown. To avoid device crash because of
    PERST# timeout, disable hot reset in pm_turnoff irq and reenable it when
    we do linktrain again.
    
    Change-Id: I87961faaae2a14c9a5e7d24543b416914076dd2e
    Signed-off-by: Qiang Yu <quic_qianyu@quicinc.com>

--- drivers/platform/msm/ep_pcie/ep_pcie_com.h
+++ drivers/platform/msm/ep_pcie/ep_pcie_com.h
@@ -1,7 +1,7 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 /*
  * Copyright (c) 2015-2021, The Linux Foundation. All rights reserved.
- * Copyright (c) 2022-2023, Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) 2022-2024, Qualcomm Innovation Center, Inc. All rights reserved.
  */
 
 #ifndef __EP_PCIE_COM_H
@@ -514,6 +514,7 @@ struct ep_pcie_dev_t {
 	u32				tcsr_perst_enable_offset;
 	u32				perst_raw_rst_status_mask;
 	u32				pcie_disconnect_req_reg_mask;
+	u32				tcsr_hot_reset_en_offset;
 };
 
 extern struct ep_pcie_dev_t ep_pcie_dev;
