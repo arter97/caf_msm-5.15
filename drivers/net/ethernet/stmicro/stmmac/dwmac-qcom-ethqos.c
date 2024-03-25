650d033278be2e466b0623f3633caf6f5908e4e8
Jinesh K. Jayakumar <quic_jineshk@quicinc.com>
2024-03-25 10:59:32 -0400

    drivers: net: stmmac: Fix serdes power down sequence
    During a system resume after XOSD, sometimes serdes is unable to
    provide clocks at the right frequency. Fix the serdes power down
    sequence as per hardware manual for cleaner serdes shutdown and
    ensure that serdes is reconfigured during next link up.
    
    Change-Id: I2d54fa90b25089e5f3f8f155a5db755296414e1d
    Signed-off-by: Jinesh K. Jayakumar <quic_jineshk@quicinc.com>

--- drivers/net/ethernet/stmicro/stmmac/dwmac-qcom-ethqos.c
+++ drivers/net/ethernet/stmicro/stmmac/dwmac-qcom-ethqos.c
@@ -6379,8 +6379,6 @@ static int ethqos_serdes_power_saving(struct net_device *ndev, void *priv,
 			ret = qcom_ethqos_enable_serdes_clocks(ethqos);
 			if (ret)
 				return -EINVAL;
-
-			qcom_ethqos_serdes_power_ctrl(ethqos, true);
 #endif
 
 			if (needs_serdes_reset)
@@ -6390,7 +6388,7 @@ static int ethqos_serdes_power_saving(struct net_device *ndev, void *priv,
 		}
 	} else {
 #if IS_ENABLED(CONFIG_ETHQOS_QCOM_VER4)
-		qcom_ethqos_serdes_power_ctrl(ethqos, false);
+		qcom_ethqos_serdes_power_down(ethqos);
 
 		qcom_ethqos_disable_serdes_clocks(ethqos);
 #endif
