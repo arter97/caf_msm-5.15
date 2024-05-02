bbc7118b17049a2fccbaa40e522a8b28b76513af
Saurav Shah <quic_sauravs@quicinc.com>
2024-05-02 15:03:43 +0530

    drivers: iio: Reduce sleep time for asm330lhh reset
    Reduce sleep time to 40ms, after asm330lhh reset. The turn on time
    for asm330lhh is 35ms based on the datasheet. Add logic to wait until
    the register is reset, and replace sleep with this for memory content
    reboot. This change is to reduce proe time, thereby improving boot KPI.
    
    Change-Id: I5e6d16431bd79b7cafdd651b4a41d022d1ec6113
    Signed-off-by: Saurav Shah <quic_sauravs@quicinc.com>

--- drivers/iio/imu/st_asm330lhh/st_asm330lhh_core.c
+++ drivers/iio/imu/st_asm330lhh/st_asm330lhh_core.c
@@ -1057,6 +1057,24 @@ static int st_asm330lhh_get_int_reg(struct st_asm330lhh_hw *hw, u8 *drdy_reg)
 	return err;
 }
 
+static int st_asm330lhh_wait_for_reg_clear(struct st_asm330lhh_hw *hw,
+				   u8 addr, u8 mask, u8 max_retry)
+{
+	u8 i = 0;
+	u8 data = 0;
+	int ret = EAGAIN;
+
+	for (i = 0; i < max_retry; i++) {
+		hw->tf->read(hw->dev, addr, sizeof(data), &data);
+		if ((data & mask) == 0) {
+			ret = 0;
+			break;
+		}
+		udelay(1000);
+	}
+	return ret;
+}
+
 static int st_asm330lhh_reset_device(struct st_asm330lhh_hw *hw)
 {
 	int err;
@@ -1074,13 +1092,18 @@ static int st_asm330lhh_reset_device(struct st_asm330lhh_hw *hw)
 	if (err < 0)
 		return err;
 
-	msleep(50);
+	msleep(40); //35ms is turn-on-time
+	st_asm330lhh_wait_for_reg_clear(hw, ST_ASM330LHH_REG_CTRL3_C_ADDR,
+					   ST_ASM330LHH_REG_SW_RESET_MASK, 10);
+
 
 	/* boot */
 	err = st_asm330lhh_write_with_mask(hw, ST_ASM330LHH_REG_CTRL3_C_ADDR,
 					   ST_ASM330LHH_REG_BOOT_MASK, 1);
 
-	msleep(50);
+	//wait until the bit is cleared
+	st_asm330lhh_wait_for_reg_clear(hw, ST_ASM330LHH_REG_CTRL3_C_ADDR,
+					   ST_ASM330LHH_REG_SW_RESET_MASK, 50);
 
 	return err;
 }
