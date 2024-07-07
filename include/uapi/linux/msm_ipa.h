CodeLinaro-mirror/kernel-platform-2-0-os_kernel_msm-5.15

dataipa: Added ioctl support for qos rules
Make interface changes to provide qos rules from
driver to ipacm.

Change-Id: I79d02ee61a1917f08f26e080dc5a46eeb8f6a3ef
Signed-off-by: Abhishek Raghuvanshi <quic_araghuva@quicinc.com>

include/uapi/linux/msm_ipa.h
@@ -154,6 +154,8 @@
 #define IPA_IOCTL_QUERY_CACHED_DRIVER_MSG       98
 #define IPA_IOCTL_ADD_DEL_DSCP_PCP_MAPPING      99
 #define IPA_IOCTL_ADD_VLAN_PRIORITY             100
+#define IPA_IOCTL_QOS_PARAM                     101
+#define IPA_IOCTL_FLUSH_QOS_PARAM               102
 /**
  * max size of the header to be inserted
  */
@@ -1067,7 +1069,15 @@ enum ipa_ipsec_ul_flt_evt {
 #define IPA_IPSEC_UL_FLT_EVENT_MAX IPA_IPSEC_UL_FLT_EVENT_MAX
 };
 
-#define IPA_EVENT_MAX_NUM (IPA_IPSEC_UL_FLT_EVENT_MAX)
+enum ipa_qos_param_evt {
+	IPA_QOS_PARAM_ADD_EVENT = IPA_IPSEC_UL_FLT_EVENT_MAX,
+	IPA_QOS_PARAM_DELETE_EVENT,
+	IPA_QOS_PARAM_FLUSH_EVENT,
+	IPA_QOS_PARAM_EVENT_MAX
+#define IPA_QOS_PARAM_EVENT_MAX IPA_QOS_PARAM_EVENT_MAX
+};
+
+#define IPA_EVENT_MAX_NUM (IPA_QOS_PARAM_EVENT_MAX)
 #define IPA_EVENT_MAX ((int)IPA_EVENT_MAX_NUM)
 
 /**
@@ -3749,6 +3759,45 @@ struct ipa_ioc_ipsec_ul_flt_attr {
 	struct ipa_rule_attrib attr;
 };
 
+enum ipa_qos_iface_category {
+	IPA_QOS_IFACE_WAN,
+	IPA_QOS_IFACE_LAN
+};
+
+
+struct ipa_ioc_qos_config
+{
+    char dev_name[IPA_RESOURCE_NAME_MAX];
+    enum ipa_qos_param_evt qos_param_evt_type;
+    enum ipa_qos_iface_category iface_cat;
+    uint8_t dir;
+    uint8_t ip_type;
+    uint8_t traffic_class;
+
+    uint32_t src_ip_addr;
+    uint32_t src_subnet;
+    uint32_t dst_ip_addr;
+    uint32_t dst_subnet;
+    uint16_t src_port_start;
+    uint16_t src_port_end;
+    uint16_t dst_port_start;
+    uint16_t dst_port_end;
+    uint8_t protocol;
+
+    uint32_t src_v6_ip_addr[4];
+    uint32_t src_v6_ip_subnet[4];
+    uint32_t dst_v6_ip_addr[4];
+    uint32_t dst_v6_ip_subnet[4];
+
+    uint8_t src_mac_addr[IPA_MAC_ADDR_SIZE];
+    uint8_t dst_mac_addr[IPA_MAC_ADDR_SIZE];
+    uint16_t vlan_count;
+    uint16_t vlan_ids[30];
+    uint8_t dscp;
+    uint8_t pcp;
+};
+
+
 /**
  *   actual IOCTLs supported by IPA driver
  */
@@ -4089,6 +4138,14 @@ struct ipa_ioc_ipsec_ul_flt_attr {
 #define IPA_IOC_ADD_VLAN_PRIORITY _IOWR(IPA_IOC_MAGIC, \
 				IPA_IOCTL_ADD_VLAN_PRIORITY, \
 				struct ipa_ioc_vlan_priority)
+
+#define IPA_IOC_QOS_PARAM _IOWR(IPA_IOC_MAGIC, \
+				IPA_IOCTL_QOS_PARAM, \
+				struct ipa_ioc_qos_config)
+
+#define IPA_IOC_FLUSH_QOS_PARAM _IOWR(IPA_IOC_MAGIC, \
+				IPA_IOCTL_FLUSH_QOS_PARAM, \
+				struct ipa_ioc_qos_config)
 
 /*
  * unique magic number of the Tethering bridge ioctls

arter97/caf_msm-5.15

include/uapi/linux/msm_ipa.h
@@ -158,7 +158,8 @@
 #define IPA_IOCTL_UPDATE_L2TP_CONFIG            102
 #define IPA_IOCTL_ADD_DEL_PDN_DSCP_MAPPING      103
 #define IPA_IOCTL_UPDATE_PDN_DSCP_MAPPING       104
-
+#define IPA_IOCTL_QOS_PARAM                     105
+#define IPA_IOCTL_FLUSH_QOS_PARAM               106
 /**
  * max size of the header to be inserted
  */
@@ -1079,7 +1080,15 @@ enum ipa_pdn_dscp_evt {
 #define IPA_PDN_DSCP_EVENT_MAX IPA_PDN_DSCP_EVENT_MAX
 };
 
-#define IPA_EVENT_MAX_NUM (IPA_PDN_DSCP_EVENT_MAX)
+enum ipa_qos_param_evt {
+	IPA_QOS_PARAM_ADD_EVENT = IPA_PDN_DSCP_EVENT_MAX,
+	IPA_QOS_PARAM_DELETE_EVENT,
+	IPA_QOS_PARAM_FLUSH_EVENT,
+	IPA_QOS_PARAM_EVENT_MAX
+#define IPA_QOS_PARAM_EVENT_MAX IPA_QOS_PARAM_EVENT_MAX
+};
+
+#define IPA_EVENT_MAX_NUM (IPA_QOS_PARAM_EVENT_MAX)
 #define IPA_EVENT_MAX ((int)IPA_EVENT_MAX_NUM)
 
 /**
@@ -3792,6 +3801,45 @@ struct ipa_ioc_ipsec_ul_flt_attr {
 	struct ipa_rule_attrib attr;
 };
 
+enum ipa_qos_iface_category {
+	IPA_QOS_IFACE_WAN,
+	IPA_QOS_IFACE_LAN
+};
+
+
+struct ipa_ioc_qos_config {
+	char dev_name[IPA_RESOURCE_NAME_MAX];
+	enum ipa_qos_param_evt qos_param_evt_type;
+	enum ipa_qos_iface_category iface_cat;
+	uint8_t dir;
+	uint8_t ip_type;
+	uint8_t traffic_class;
+
+	uint32_t src_ip_addr;
+	uint32_t src_subnet;
+	uint32_t dst_ip_addr;
+	uint32_t dst_subnet;
+	uint16_t src_port_start;
+	uint16_t src_port_end;
+	uint16_t dst_port_start;
+	uint16_t dst_port_end;
+	uint8_t protocol;
+
+	uint32_t src_v6_ip_addr[4];
+	uint32_t src_v6_ip_subnet[4];
+	uint32_t dst_v6_ip_addr[4];
+	uint32_t dst_v6_ip_subnet[4];
+
+	uint8_t src_mac_addr[IPA_MAC_ADDR_SIZE];
+	uint8_t dst_mac_addr[IPA_MAC_ADDR_SIZE];
+	uint16_t vlan_count;
+	uint16_t vlan_ids[30];
+	uint8_t dscp;
+	uint8_t pcp;
+	uint8_t dscp_mark_val;
+};
+
+
 /**
  *   actual IOCTLs supported by IPA driver
  */
@@ -4148,6 +4196,14 @@ struct ipa_ioc_ipsec_ul_flt_attr {
 #define IPA_IOC_UPDATE_L2TP_CONFIG _IOW(IPA_IOC_MAGIC, \
 				IPA_IOCTL_UPDATE_L2TP_CONFIG, \
 				uint32_t)
+
+#define IPA_IOC_QOS_PARAM _IOWR(IPA_IOC_MAGIC, \
+				IPA_IOCTL_QOS_PARAM, \
+				struct ipa_ioc_qos_config)
+
+#define IPA_IOC_FLUSH_QOS_PARAM _IOWR(IPA_IOC_MAGIC, \
+				IPA_IOCTL_FLUSH_QOS_PARAM, \
+				struct ipa_ioc_qos_config)
 
 /*
  * unique magic number of the Tethering bridge ioctls