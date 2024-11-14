1247563e3b0f22ec1caf590ef776c31ad529580c
Bhasker Reddy Komatireddy <quic_kbhasker@quicinc.com>
2024-11-14 11:46:38 +0530

    driver: update hdmi_in/typec logic signal
    change hdmi_in/typec logic signal in hdmi input mux driver.
    
    Change-Id: Ie9a2926643ca9a09a257cdbdddbcc5b7900c3011
    Signed-off-by: Bhasker Reddy Komatireddy <quic_kbhasker@quicinc.com>
    (cherry picked from commit b550be8fc3b62a054585ef2ee44e8470093ad808)

--- drivers/misc/hdmi_input_mux.c
+++ drivers/misc/hdmi_input_mux.c
@@ -17,8 +17,9 @@
 #include <linux/of_irq.h>
 #include <linux/extcon.h>
 
-#define HDMI_INPUT_HDMI_IN 0
-#define HDMI_INPUT_TYPEC 1
+#define HDMI_INPUT_HDMI_IN 1
+#define HDMI_INPUT_TYPEC 0
+
 enum hdmi_input_t {
 	NONE,
 	TYPEC,
