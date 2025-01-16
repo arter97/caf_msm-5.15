15184d658526ccc571d84ba753cc53a274ea9afe
Sumit Kumar <quic_sumk@quicinc.com>
2025-01-16 23:00:38 +0530

    Revert "msm: mhi_dev: Defer creation of /dev/mhi_ctrl till link is up"
    This reverts commit c8db6368c42377ed44d5f1c49a506902f2e47794.
    
    Reason: Following the changes <58f6bd276d8f> ("msm: ep_pcie: Set link
    status invalid and register device on usb boot") and <9de7c41f8dbd>
    ("msm: mhi_dev: check for link invalid and return on usb boot")
    there is no need to defer the /dev/mhi_ctrl till link up because
    mhi probe will check for ep pcie link status and will only proceed
    further if the status is not invalid.
    
    Change-Id: Ic4366c83162c91529b7c8b394d8f4dca411a2236
    Signed-off-by: Sumit Kumar <quic_sumk@quicinc.com>

--- drivers/platform/msm/mhi_dev/mhi.c
+++ drivers/platform/msm/mhi_dev/mhi.c
@@ -5626,6 +5626,7 @@ static int mhi_dev_probe(struct platform_device *pdev)
 		INIT_LIST_HEAD(&mhi_pf->client_cb_list);
 		mutex_init(&mhi_pf->mhi_lock);
 
+		mhi_uci_init();
 		mhi_update_state_info(mhi_pf, MHI_STATE_CONFIGURED);
 	}
 
@@ -5635,17 +5636,12 @@ static int mhi_dev_probe(struct platform_device *pdev)
 		mhi_dev_pcie_notify_event = MHI_INIT;
 		/* Get EP PCIe capabilities to check if it supports SRIOV capability */
 		ep_pcie_core_get_capability(mhi_hw_ctx->phandle, &mhi_hw_ctx->ep_cap);
-
-		/*
-		 * PCIe driver is available and link is already up,
-		 * proceed with UCI initialization
-		 */
-		mhi_uci_init();
 		/*
 		 * Setup all virtual device prior to PF Mission mode
 		 * completion to make sure VF's are initialized in mission
 		 * mode directly, if not host assumes it in PBL state.
 		 */
+
 		if (!mhi_pf) {
 			mhi_log(MHI_DEFAULT_ERROR_LOG_ID, MHI_MSG_ERROR,
 					"mhi_pf is NULL, defering\n");
