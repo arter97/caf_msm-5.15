4dd46728f5a915ad3d37494a8a2f45381fa43d19
Kumar Anurag Singh <quic_kumaranu@quicinc.com>
2025-02-26 04:24:44 -0800

    Revert "ASoC: codecs: tas5805m: Modify i2c actions move to prepare function"
    This reverts commit 2bc5acac43c636a4ab8c08e237ba62fdecb6bc0a.
    
    Reason for revert: <Not required on release branch due to relation with other changes.>
    
    Change-Id: I414c749fb2eb467dc599a5879aabe0974fa4ecf9
    Signed-off-by: Kumar Anurag Singh <quic_kumaranu@quicinc.com>

--- sound/soc/codecs/tas5805m.c
+++ sound/soc/codecs/tas5805m.c
@@ -299,7 +299,6 @@ struct TAS5805m_priv {
 	int hs_state;
 
 	int mute;
-	bool prepare_in_dai;
 };
 
 const struct regmap_config TAS5805m_regmap = {
@@ -700,23 +699,8 @@ static void tas5805m_priv_init(struct TAS5805m_priv *priv)
 	}
 }
 
-static int TAS5805m_prepare(struct snd_pcm_substream *substream,
-	struct snd_soc_dai *dai)
-{
-	struct TAS5805m_priv *priv = snd_soc_component_get_drvdata(dai->component);
-	(void)substream;
-
-	if (!priv->init_done && priv->prepare_in_dai) {
-		dev_err(&priv->client->dev, "prepare_in_dai\n");
-		tas5805m_priv_init(priv);
-	}
-
-	return 0;
-}
-
 static const struct snd_soc_dai_ops TAS5805m_dai_ops = {
 	.mute_stream = TAS5805m_mute,
-	.prepare = TAS5805m_prepare,
 };
 
 static struct snd_soc_dai_driver TAS5805m_dai = {
@@ -785,15 +769,11 @@ static int TAS5805m_probe(struct i2c_client *client, struct regmap *regmap)
 	/* pull /PDN high */
 	gpio_set_value(priv->pdn_gpio, 1);
 
-	priv->prepare_in_dai = of_property_read_bool(np, "prepare-in-dai");
-	dev_err(dev, "prepare_in_dai = %d\n", priv->prepare_in_dai);
-	if (!priv->prepare_in_dai) {
-		priv->init_done = 0;
-		tas5805m_priv_init(priv);
-		if (priv->init_done == 0) {
-			dev_err(dev, "Failed to initialize the tas5805 device!\n");
-			goto config_err;
-		}
+	priv->init_done = 0;
+	tas5805m_priv_init(priv);
+	if (priv->init_done == 0) {
+		dev_err(dev, "Failed to initialize the tas5805 device!\n");
+		goto config_err;
 	}
 
 	mutex_init(&priv->lock);
