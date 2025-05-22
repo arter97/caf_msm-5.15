e7f1f6cdad53c9c0b9022695fb963698eea41003
Khaja Hussain Shaik Khaji <quic_kshaikkh@quicinc.com>
2025-05-22 18:38:18 +0530

    drivers: remoteproc: Send SSR state change uevents via sysfs
    Add support for emitting uevents during SSR transitions in Qualcomm
    Remoteproc.
    
    Introduce uevent emission from the Qualcomm Remoteproc subsystem during
    Subsystem Restart (SSR) state transitions. Use kobject_uevent_env() to
    send uevents via sysfs, including the SSR action (e.g.,
    QCOM_SSR_BEFORE_POWERUP, QCOM_SSR_AFTER_SHUTDOWN, etc.).
    
    Enable user space to monitor SSR lifecycle events and respond
    accordingly. This is useful for:
    - Logging and diagnostics
    - Triggering user-space recovery mechanisms
    - Integrating with system monitoring tools
    
    Add a helper function qcom_rproc_send_ssr_uevent() and integrate it
    into the SSR notification flow. The implementation is lightweight and
    leverages existing kernel infrastructure.
    
    Change-Id: I9f7b20ea1657eb4fe24a5fe008dfe4b88c27eea2
    Signed-off-by: Khaja Hussain Shaik Khaji <quic_kshaikkh@quicinc.com>

--- drivers/remoteproc/qcom_common.h
+++ drivers/remoteproc/qcom_common.h
@@ -1,7 +1,7 @@
 /* SPDX-License-Identifier: GPL-2.0 */
 /*
  * Copyright (c) 2020-2021, The Linux Foundation. All rights reserved.
- * Copyright (c) 2022-2024, Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
  */
 #ifndef __RPROC_QCOM_COMMON_H__
 #define __RPROC_QCOM_COMMON_H__
@@ -60,6 +60,7 @@ struct qcom_rproc_ssr {
 	enum qcom_ssr_notify_type notification;
 	struct timer_list timer;
 	struct qcom_ssr_subsystem *info;
+	struct rproc *rproc;
 };
 
 extern bool qcom_device_shutdown_in_progress;
