b493dd40bb3ccca2c4b65d34ae1e1fbfed43ad6a
Xueshi Shi <quic_xuesshi@quicinc.com>
2025-06-03 03:07:54 -0700

    Revert "usb: dwc3-msm: Reset utmi sleep assert during resume"
    This reverts commit bac9985cb21b71aa8503bd204c90229680525f04.
    
    Change-Id: I1a6e457e94eee55fab77782cb8b2f3687bdac320
    Signed-off-by: Xueshi Shi <quic_xuesshi@quicinc.com>

--- drivers/usb/dwc3/dwc3-msm-core.c
+++ drivers/usb/dwc3/dwc3-msm-core.c
@@ -4088,7 +4088,6 @@ static int dwc3_msm_resume(struct dwc3_msm *mdwc)
 	int ret;
 	struct dwc3 *dwc = NULL;
 	struct usb_irq *uirq;
-	u32 reg = 0;
 
 	if (mdwc->dwc3)
 		dwc = platform_get_drvdata(mdwc->dwc3);
@@ -4199,10 +4198,10 @@ static int dwc3_msm_resume(struct dwc3_msm *mdwc)
 	/* enable power evt irq for IN P3 detection */
 	enable_irq(mdwc->wakeup_irq[PWR_EVNT_IRQ].irq);
 
-	/* Disable HSPHY auto suspend and utmi sleep assert */
-	reg = dwc3_msm_read_reg(mdwc->base, DWC3_GUSB2PHYCFG(0));
+	/* Disable HSPHY auto suspend */
 	dwc3_msm_write_reg(mdwc->base, DWC3_GUSB2PHYCFG(0),
-		reg & ~DWC3_GUSB2PHYCFG_SUSPHY & ~DWC3_GUSB2PHYCFG_ENBLSLPM);
+		dwc3_msm_read_reg(mdwc->base, DWC3_GUSB2PHYCFG(0)) &
+				~DWC3_GUSB2PHYCFG_SUSPHY);
 
 	/* Disable wakeup capable for HS_PHY IRQ & SS_PHY_IRQ if enabled */
 	if (mdwc->lpm_flags & MDWC3_ASYNC_IRQ_WAKE_CAPABILITY) {
