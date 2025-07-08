40744425194216ee5c9d783609b2de79730537b9
Manish Nagar <mnagar@qti.qualcomm.com>
2025-07-08 18:09:56 +0530

    usb: dwc3-msm: Add null check for dwc in vbus_notifier
    Add a null pointer check on dwc before access the
    component of dwc to avoid null pointer dereference.
    
    Change-Id: I76c89bdde21a5e8e5a55521e927903104936bbea
    Signed-off-by: Manish Nagar <mnagar@qti.qualcomm.com>

--- drivers/usb/dwc3/dwc3-msm-core.c
+++ drivers/usb/dwc3/dwc3-msm-core.c
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /*
  * Copyright (c) 2012-2021, The Linux Foundation. All rights reserved.
- * Copyright (c) 2022-2025 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
  */
 
 #include <linux/module.h>
@@ -4807,7 +4807,7 @@ static int dwc3_msm_vbus_notifier(struct notifier_block *nb,
 		 * If bus suspend feature is enabled, increase the autosuspend delay to default,
 		 * so that the HS-USB re-enumeration isn't interrupted by dwc3 RT suspend.
 		 */
-			if (!event && dwc->runtime_suspend_on_usb_suspend)
+			if (dwc && !event && dwc->runtime_suspend_on_usb_suspend)
 				pm_runtime_set_autosuspend_delay(dwc->dev,
 						DWC3_DEFAULT_AUTOSUSPEND_DELAY);
 			dwc3_override_vbus_status(mdwc, !!event);
