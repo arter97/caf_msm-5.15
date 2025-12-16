442b404049c109a8107115c2a2cf210dafa7ea3f
Tianzhen Wang <quic_tianzhen@quicinc.com>
2025-12-16 15:12:23 +0530

    Revert "mtd: msm_qpic_nand: check for page_erased bit along with op_err" in order to solve AU#783 upmerge conflict This reverts commit 639811afadf855d5d1fb1a4996eb270fc3d85366.
    Change-Id: I3e4645d6f55e64b888ad49407585e35dc2f7eb50
    Signed-off-by: Tianzhen Wang <tianzhen@qti.qualcomm.com>

--- drivers/mtd/devices/msm_qpic_nand.c
+++ drivers/mtd/devices/msm_qpic_nand.c
@@ -2,7 +2,7 @@
 /*
  * Copyright (C) 2007 Google, Inc.
  * Copyright (c) 2012-2021 The Linux Foundation. All rights reserved.
- * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+ * Copyright (c) 2022-2024 Qualcomm Innovation Center, Inc. All rights reserved.
  */
 
 #include "msm_qpic_nand.h"
@@ -2267,7 +2267,7 @@ free_dma:
 			if (last_pos < ecc_bytes_percw_in_bits)
 				num_zero_bits++;
 
-			if (num_zero_bits > MAX_ECC_BIT_FLIPS) {
+			if (num_zero_bits > 4) {
 				*erased_page = false;
 				goto free_mem;
 			}
@@ -2279,7 +2279,7 @@ free_dma:
 		ecc_temp += chip->ecc_parity_bytes;
 	}
 
-	if ((n == cwperpage) && (num_zero_bits <= MAX_ECC_BIT_FLIPS))
+	if ((n == cwperpage) && (num_zero_bits <= 4))
 		*erased_page = true;
 free_mem:
 	kfree(ecc);
@@ -2482,33 +2482,6 @@ static int msm_nand_read_pagescope(struct mtd_info *mtd, loff_t from,
 			goto free_dma;
 		/* Check for flash status errors */
 		pageerr = rawerr = 0;
-
-		/*
-		 * PAGE_ERASED bit will set only if all
-		 * CODEWORD_ERASED bit of all codewords
-		 * of the page is set.
-		 *
-		 * PAGE_ERASED bit is a 'logical and' of all
-		 * CODEWORD_ERASED bit of all codewords i.e.
-		 * even if one codeword is detected as not
-		 * an erased codeword, PAGE_ERASED bit will unset.
-		 */
-		for (n = rw_params.start_sector; n < cwperpage; n++) {
-			if ((dma_buffer->result[n].erased_cw_status &
-					(1 << PAGE_ERASED)) &&
-					(dma_buffer->result[n].buffer_status &
-					 NUM_ERRORS)) {
-				err = msm_nand_is_erased_page_ps(mtd,
-						from, ops,
-						&rw_params,
-						&erased_page);
-				if (err)
-					goto free_dma;
-				if (erased_page)
-					rawerr = -EIO;
-				break;
-			}
-		}
 		for (n = rw_params.start_sector; n < cwperpage; n++) {
 			if (dma_buffer->result[n].flash_status & (FS_OP_ERR |
 					FS_MPU_ERR)) {
@@ -2863,33 +2836,6 @@ static int msm_nand_read_multipage(struct mtd_info *mtd, loff_t from,
 
 		/* Check for flash status errors */
 		pageerr = rawerr = 0;
-
-		/*
-		 * PAGE_ERASED bit will set only if all
-		 * CODEWORD_ERASED bit of all codewords
-		 * of the page is set.
-		 *
-		 * PAGE_ERASED bit is a 'logical and' of all
-		 * CODEWORD_ERASED bit of all codewords i.e.
-		 * even if one codeword is detected as not
-		 * an erased codeword, PAGE_ERASED bit will unset.
-		 */
-		for (n = rw_params.start_sector; n < (cwperpage * mp_to_read); n++) {
-			if ((dma_buffer->result[n].erased_cw_status &
-					(1 << PAGE_ERASED)) &&
-					(dma_buffer->result[n].buffer_status &
-					 NUM_ERRORS)) {
-				err = msm_nand_is_erased_page_ps(mtd,
-						from, ops,
-						&rw_params,
-						&erased_page);
-				if (err)
-					goto free_dma;
-				if (erased_page)
-					rawerr = -EIO;
-				break;
-			}
-		}
 		for (n = rw_params.start_sector; n < (cwperpage * mp_to_read); n++) {
 			if (dma_buffer->result[n].flash_status & (FS_OP_ERR |
 					FS_MPU_ERR)) {
@@ -3270,7 +3216,7 @@ free_dma:
 			if (last_pos < ecc_bytes_percw_in_bits)
 				num_zero_bits++;
 
-			if (num_zero_bits > MAX_ECC_BIT_FLIPS) {
+			if (num_zero_bits > 4) {
 				*erased_page = false;
 				goto free_mem;
 			}
@@ -3282,7 +3228,7 @@ free_dma:
 		ecc_temp += chip->ecc_parity_bytes;
 	}
 
-	if ((n == cwperpage) && (num_zero_bits <= MAX_ECC_BIT_FLIPS))
+	if ((n == cwperpage) && (num_zero_bits <= 4))
 		*erased_page = true;
 free_mem:
 	kfree(ecc);
@@ -3462,33 +3408,6 @@ static int msm_nand_read_oob(struct mtd_info *mtd, loff_t from,
 			goto free_dma;
 		/* Check for flash status errors */
 		pageerr = rawerr = 0;
-
-		/*
-		 * PAGE_ERASED bit will set only if all
-		 * CODEWORD_ERASED bit of all codewords
-		 * of the page is set.
-		 *
-		 * PAGE_ERASED bit is a 'logical and' of all
-		 * CODEWORD_ERASED bit of all codewords i.e.
-		 * even if one codeword is detected as not
-		 * an erased codeword, PAGE_ERASED bit will unset.
-		 */
-		for (n = rw_params.start_sector; n < cwperpage; n++) {
-			if ((dma_buffer->result[n].erased_cw_status &
-					(1 << PAGE_ERASED)) &&
-					(dma_buffer->result[n].buffer_status &
-					 NUM_ERRORS)) {
-				err = msm_nand_is_erased_page(mtd,
-						from, ops,
-						&rw_params,
-						&erased_page);
-				if (err)
-					goto free_dma;
-				if (erased_page)
-					rawerr = -EIO;
-				break;
-			}
-		}
 		for (n = rw_params.start_sector; n < cwperpage; n++) {
 			if (dma_buffer->result[n].flash_status & (FS_OP_ERR |
 					FS_MPU_ERR)) {
