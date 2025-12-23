2f066d222f2510829f462f5b25a513eda1723593
Veena Sri Vallala <vsri@qti.qualcomm.com>
2025-12-23 12:25:28 +0530

    stmmac: Enabling power optimization for sdxbaagha
    Enabling power optimization for sdxbaagha.
    
    Change-Id: I427f8b89a740ff7e6e8ee32a7910fe510c9303c3
    Signed-off-by: Veena Sri Vallala <vsri@qti.qualcomm.com>

--- drivers/net/ethernet/stmicro/stmmac/dwmac-qcom-ethqos.h
+++ drivers/net/ethernet/stmicro/stmmac/dwmac-qcom-ethqos.h
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 /* Copyright (c) 2021, The Linux Foundation. All rights reserved.
- * Copyright (c) 2022-2024 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
  */
 #ifndef	_DWMAC_QCOM_ETHQOS_H
 #define	_DWMAC_QCOM_ETHQOS_H
@@ -525,13 +525,12 @@ struct qcom_ethqos {
 	struct mac_csr_data *mac_reg_list;
 	bool power_state;
 	bool gdsc_off_on_suspend;
-#if IS_ENABLED(CONFIG_ETHQOS_QCOM_VER4)
+
 	/* Protects clocks and reference count */
 	struct mutex ps_lock;
 	refcount_t ps_refcount;
 	bool clk_active;
 	bool clk_gated;
-#endif
 };
 
 struct pps_cfg {
