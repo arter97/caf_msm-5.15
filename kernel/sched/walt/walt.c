ecaca75c44910957441b6ad7d6b2cceb5c27ccfa
Ankur Garg <ankugarg@qti.qualcomm.com>
2026-03-23 17:20:26 +0530

    sched/walt: Remove QUIC copyrights for walt
    Change-Id: I454bb9827006d3886fd7d816a9bc27644c3f08ac
    Signed-off-by: Ankur Garg <ankugarg@qti.qualcomm.com>

--- kernel/sched/walt/walt.c
+++ kernel/sched/walt/walt.c
@@ -1,8 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /*
  * Copyright (c) 2016-2021, The Linux Foundation. All rights reserved.
- * Copyright (c) 2022-2023, Qualcomm Innovation Center, Inc. All rights reserved.
- * Copyright (c) 2026, Qualcomm Technologies, Inc. and/or its subsidiaries. All rights reserved.
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
  */
 
 #include <linux/syscore_ops.h>
